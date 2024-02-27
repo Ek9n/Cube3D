e734574deec732661e0551e892af03888c506231
Hannes Stein <hstein@c4b5c4.42berlin.de>
2024-02-27 15:02:06 +0100

    sachenCommitted

--- src/execute/run_game.c
+++ src/execute/run_game.c
@@ -6,7 +6,7 @@
 /*   By: hstein <hstein@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 13:41:09 by hstein            #+#    #+#             */
-/*   Updated: 2024/02/27 14:39:40 by hstein           ###   ########.fr       */
+/*   Updated: 2024/02/27 14:53:33 by hstein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 void run_game(t_data *data)
 {
+    printf("hi :-)");
     mlx_init_data(data);
-    printf("hi");
+
+    // mlx_destroy_window(data->mlx, data->mlx_win);
+    mlx_loop(data->mlx);
 }
+/*
+		mlx_loop_hook(data.mlx_ptr, &render, &data);
+		mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+		mlx_mouse_hook(data.win_ptr, &handle_mouse, &data);
+		mlx_hook(data.win_ptr, 17, 1L << 17, close_window, &data);
+		mlx_loop(data.mlx_ptr);
+*/
