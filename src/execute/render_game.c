fce27853e7acf3159ca1efd2c1abcb40bb7f66ea
Hannes Stein <hstein@c3a4c5.42berlin.de>
2024-04-18 16:37:40 +0200

    imgsizerot

--- src/execute/render_game.c
+++ src/execute/render_game.c
@@ -6,7 +6,7 @@
 /*   By: hstein <hstein@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 13:58:23 by jborner           #+#    #+#             */
-/*   Updated: 2024/04/18 16:31:53 by hstein           ###   ########.fr       */
+/*   Updated: 2024/04/18 16:37:31 by hstein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -138,15 +138,15 @@ int	render(t_data *data)
 	// put_img_to_img(data->texture->base_img, data->texture->minimap->resize, 10,
 	// 	10);
 	put_img_to_img(data->texture->base_img, data->texture->minimap->resize, 1500, 600);
-	rotate_image(data, &data->texture->steeringwheel, 0);
-		// static int i = 0;
-		// if (i > 360)
-		// 	i = 0;
-		// if (i % 30 == 0)
-		// {
-		// 	rotate_image(data, &data->texture->steeringwheel, i);
-		// }
-		// 	i += 10;
+	// rotate_image(data, &data->texture->steeringwheel, 0);
+		static int i = 0;
+		if (i > 360)
+			i = 0;
+		if (i % 30 == 0)
+		{
+			rotate_image(data, &data->texture->steeringwheel, i);
+		}
+			i += 10;
 		
 	put_img_to_img(data->texture->base_img, data->texture->steeringwheel2, 800, 800);
 	mlx_put_image_to_window(data->mlx, data->mlx_win,
