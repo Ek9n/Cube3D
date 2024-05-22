f342cd659f4a1b0af739ab979530000d9ca9dc33
Jessica Borner <jborner@c4b6c6.42berlin.de>
2024-05-22 16:53:05 +0200

    adjust rotation speed

--- src/control/key_action.c
+++ src/control/key_action.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   key_action.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hstein <hstein@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jborner <jborner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 08:35:01 by yubi42            #+#    #+#             */
-/*   Updated: 2024/05/15 16:17:47 by hstein           ###   ########.fr       */
+/*   Updated: 2024/05/22 16:50:55 by jborner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,9 +51,6 @@ void	move_player(t_data *data, int sign, int num, int other_num)
 	calc_speed(data, sign);
 }
 
-// printf("rotation %i\n", data->player->rotation);
-// printf("cur speed: %i km/h\n", (int)data->player->speed[0] * 10);
-
 void	rotate_player(t_data *data, int sign, int num)
 {
 	if (!data->player->dead && data->keys[num] && data->rot[num] < ROT_MAX)
@@ -66,11 +63,7 @@ void	rotate_player(t_data *data, int sign, int num)
 	if (data->rot[num] > ROT_MIN && (data->rot[XK_Up] > MOV_MIN
 			|| data->rot[XK_Down] > MOV_MIN))
 	{
-		data->player->angle += (0.000003 * (data->rot[num]
-					+ (data->player->speed[0] / 1.4) + 10) * (data->rot[num]
-					+ (data->player->speed[0] / 1.4) + 10)
-				* data->player->rev_speed * data->player->rev_speed) * sign;
-		if (data->player->angle < 0 || data->player->angle >= 2 * PI)
+data->player->angle += (0.01  + (0.00002 * data->player->rev_speed * data->rot[num] * data->rot[num] * data->player->rev_speed)) * sign;		if (data->player->angle < 0 || data->player->angle >= 2 * PI)
 			data->player->angle -= 2 * PI * sign;
 		data->player->x_sin = sin(data->player->angle);
 		data->player->y_cos = cos(data->player->angle);
